79c0d7e0451f069d1248463c86b1382ae79d0bf8
HUAYAPA <contactohuayapa@gmail.com>
2025-09-01 08:22:07 -0500

    Add models and migrate tables

---
+++ app/Models/Diagnoses.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnoses extends Model
+{
+    /** @use HasFactory<\Database\Factories\DiagnosesFactory> */
+    use HasFactory;
+}
